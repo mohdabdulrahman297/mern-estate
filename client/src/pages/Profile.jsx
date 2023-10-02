aac9275baf7230e8fdee7c7fd0d7d13d6651cb68
abdulrahmanmohd297 <abdulrahmanmohd297@gmail.com>
2023-10-02 22:31:20 +0530

    complete delete user listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -146,7 +146,24 @@ const handleShowLisitngs = async () => {
   } catch (error) {
     setShowListingsError(true);
   }
-}
+};
+
+const handleListingDelete = async (listingId) => {
+  try {
+    const res = await fetch(`/api/listing/delete/${listingId}`, {
+      method: 'DELETE',
+    });
+    const data = await res.json();
+    if(data.success === false){
+      console.log(data.message);
+      return;
+    }
+
+    setUserListings((prev) => prev.filter((listing) => listing._id !== listingId));
+  } catch (error) {
+    console.log(data.message);
+  }
+};
 
 return (
   <div className='p-3 max-w-lg mx-auto'>
@@ -197,7 +214,7 @@ return (
           </Link>
 
           <div className='flex flex-col items-center'>
-            <button className='text-red-700 uppercase'>Delete</button>
+            <button onClick={() => handleListingDelete(listing._id)} className='text-red-700 uppercase'>Delete</button>
             <button className='text-green-700 uppercase'>Edit</button>
           </div>
         </div>
