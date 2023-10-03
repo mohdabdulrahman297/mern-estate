25aa37b368e8f5e4aea1bfdbf2e75914eed3b4aa
abdulrahmanmohd297 <abdulrahmanmohd297@gmail.com>
2023-10-03 07:42:28 +0530

    complete update listing functionality

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,3 +1,4 @@
+import { error } from "console";
 import Listing from "../models/listing.model.js";
 import { errorHandler } from "../utils/error.js";
 
@@ -40,12 +41,25 @@ export const updateListing = async (req, res, next) => {
     }
 
     try {
-        const updateListing = await Listing.findByIdAndUpdate(
+        const updatedListing = await Listing.findByIdAndUpdate(
             req.params.id,
             req.body,
             {new: true}
         );
-        res.status(200).json(updateListing);
+        res.status(200).json(updatedListing);
+    } catch (error) {
+        next(error);
+    }
+};
+
+
+export const getListing = async (req, res, next) => {
+    try {
+        const listing = await Listing.findById(req.params.id);
+        if(!listing){
+            return next(errorHandler(401, 'Listing not found'));
+        }
+        res.status(200).json(listing);
     } catch (error) {
         next(error);
     }
