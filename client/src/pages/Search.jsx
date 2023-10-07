508cbd91d6e498ebbeb37fc602a435979bf391a8
abdulrahmanmohd297 <abdulrahmanmohd297@gmail.com>
2023-10-07 10:33:31 +0530

    Create the listing item component and show listings

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState} from 'react'
 import { useNavigate } from 'react-router-dom';
+import Listingitem from '../components/Listingitem';
 
 
 export default function Search() {
@@ -16,7 +17,7 @@ export default function Search() {
 
     const [loading, setLoading] = useState(false);
     const [listings, setListings] = useState([]);
-    console.log(listings);
+    
     
 useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
@@ -180,8 +181,22 @@ const handleSubmit = (e) => {
 
             </form>
         </div>
-        <div className='text-3xl font-semibold border-b p-3 text-slate-700 mt-5'>
-            <h1>Listing results:</h1>
+        <div className='flex-1'>
+            <h1 className='text-3xl font-semibold border-b p-3 text-slate-700 mt-5'>Listing results:</h1>
+            <div className='p-7 flex flex-wrap gap-4'>
+                {!loading && listings.length === 0 && (
+                    <p className='text-xl text-slate-700'>No listing Found!</p>
+                )}
+                {loading && (
+                    <p className='text-xl text-slate-700 text-center w-full'>Loading...</p>
+                )
+                }
+                {!loading && 
+                  listings &&
+                  listings.map((listing) => (
+                    <Listingitem key={listing._id} listing={listing} />
+                  ))}
+            </div>
         </div>
     </div>
   )
