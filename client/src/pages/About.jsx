b2e4d44c7e06af73597d93ef27be8bda252af75c
abdulrahmanmohd297 <abdulrahmanmohd297@gmail.com>
2023-10-07 13:10:15 +0530

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from 'react';
 
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Rahman Estate</h1>
+      <p className='mb-4 text-slate-700'>Rahman Estate is your trusted partner in the world of real estate, dedicated to helping you find your dream home, sell your property, or secure the perfect rental. Our team of seasoned professionals is committed to delivering exceptional service, ensuring that your real estate journey is a seamless and rewarding one.</p>
+      <p className='mb-4 text-slate-700'>
+        At Rahman Estate, we believe in empowering our clients with expert guidance, tailored solutions, and an intimate knowledge of the local property landscape. Whether you're a buyer, seller, or looking to rent, our mission is to guide you towards success.
+      </p>
+      <p className='mb-4 text-slate-700'>Our team comprises industry experts who bring a wealth of experience and insights to the table. We are driven by the belief that real estate transactions should be both exciting and enriching. With Rahman Estate, you can trust that your property goals are in capable hands, and we're here to transform them into reality.</p>
+    </div>
+  );
 }
