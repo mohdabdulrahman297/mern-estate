d0b2a5b7cd9a7999a41007e67e28d2c23cbbd4bb
abdulrahmanmohd297 <abdulrahmanmohd297@gmail.com>
2023-10-07 13:43:51 +0530

    fixed signup.jsx file for oauth

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Link, useNavigate} from 'react-router-dom';
 import { useState } from 'react';
-import OAuth from '../components/Oauth';
+import OAuth from '../components/OAuth';
 
 
 export default function SignUp() {
